2192c14f776be66a61792d58740f1ba2f2f79d5d
Timothy <>
2020-11-22 16:38:43 -0500

    divide works

--- RationalNumber.java
+++ RationalNumber.java
@@ -121,7 +121,8 @@ public class RationalNumber extends RealNumber
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    RationalNumber a = new RationalNumber(getNumerator()*other.getDenominator(),getDenominator()*other.getNumerator());
+    return a;
   }
 
   /**
